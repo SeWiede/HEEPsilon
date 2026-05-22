3298b8e83e7e51fd91d3dcb895839c31eef4c249
Sebastian Wiedemann <sebastian.wiedemann@tuwien.ac.at>
2026-05-22 16:02:42 +0200

    sim: fix DPI shared library paths for v1.05 vendor restructure
    v1.05 renamed vendor subdirs from flat underscore form
    (lowrisc_opentitan/, pulp_platform_pulpissimo/) to nested
    (lowrisc/opentitan/, pulp_platform/pulpissimo/). Update
    the three affected paths: heepsilon.core uartdpi fileset,
    heepsilon.core vsim -sv_lib options, and both DPI compile
    scripts.

--- scripts/sim/compile_remote_bitbang.sh
+++ scripts/sim/compile_remote_bitbang.sh
@@ -1,4 +1,4 @@
-cd ../../../hw/vendor/esl_epfl_x_heep/hw/vendor/pulp_platform_pulpissimo/rtl/tb/remote_bitbang
+cd ../../../hw/vendor/esl_epfl_x_heep/hw/vendor/pulp_platform/pulpissimo/rtl/tb/remote_bitbang
 make clean
 make all
 cd -
